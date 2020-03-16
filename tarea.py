63eff98deabaee664d706ef66bb33689da955e2f
LuisBauti <luisbautista551@gmail.com>
2020-03-15 21:20:45 -0600

    Creacion de la clase mensajeria y la definicion de las funcionalidades del menu

--- tarea.py
+++ tarea.py
@@ -7,3 +7,17 @@
 import sys
 import plataform
 import os
+
+class Mensajeria(self):
+    """Recibira los mensajes y notificara al usuario""""
+    def __init__(self):
+        self.mensajes =list()
+
+        self.Opciones = {"1": self.add_Mensaje,
+                         "2": self.notificar_mensaje,
+                         "3": self.mensaje_sin_leer,
+                         "4": self.Ver_mensaje,
+                         "5": self.borrar_mensaje,
+                         "6": self.salir}
+
+    def menu(self):
