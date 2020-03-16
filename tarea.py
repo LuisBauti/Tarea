f2af5d94d7531764d79c70bab2881478a2ec8a80
LuisBauti <luisbautista551@gmail.com>
2020-03-15 21:05:33 -0600

    Commit inicial

---
+++ tarea.py
@@ -0,0 +1,9 @@
+# -*- Coding: UTF-8 -*-
+# Programa: BAndeja de mensajes
+# objetivo: Su funcion es recibir, notificar y revidsar los mensajes.
+# Autor: Luis Enrique Consuegra
+# Fecha: 15/03/2020
+
+import sys
+import plataform
+import os
